bcbcbf675248483e4a77e3288febb63f73ebfb17
CeeM-UN <cepia@unal.edu.co>
2023-12-04 16:50:30 -0500

    Squashed commit of the following:
    commit 92ae8aad12e8852aeac49871167bcafe17d57c6f
    Author: CeeM <66186846+CeeM01@users.noreply.github.com>
    Date:   Sat Dec 2 01:17:01 2023 -0500
    
        Mejoramiento de la pagina web
        limpieza de archivos innecesarios
    
    commit 5c4cda5480648b750fdd0d25833daf606a1ccc5d
    Author: CeeM <66186846+CeeM01@users.noreply.github.com>
    Date:   Sun Nov 26 17:11:22 2023 -0500
    
        creacion de grafos
    
        falta;
    
        implementar el agregar y eliminar nodo en el grafo
        implementar pantalla de inciio/bienvenida
        implementar pantalla de arboles
        mejoramieto de la interfaz grafica

--- src/componentes/Grafos.js
+++ src/componentes/Grafos.js
@@ -1,18 +1,64 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import Homepage from '../Pages/Homepage';
-import Linealespage from '../Pages/Linealespage';
-import Arbolpage from '../Pages/Arbolpage';
-import ArrayComponent from './Lineales';
-import ArrayImage from '../Images/array.jpg';
+import React from 'react';
+import { Graph } from 'react-d3-graph';
+import Imgnodo from '../Images/Imgnodo.png';
 
+const storeNames = ['Tienda 1', 'Tienda 2', 'Tienda 3', 'Tienda 4', 'Tienda 5', 'Tienda 6', 'Tienda 7', 'Tienda 8', 'Tienda 9', 'Tienda 10', 'Tienda 11', 'Tienda 12', 'Tienda 13', 'Tienda 14', 'Tienda 15', 'Tienda 16', 'Tienda 17', 'Tienda 18', 'Tienda 19', 'Tienda 20'];
 
-
-
-const Grafos = () => {
-    return (
-    <div> Hello World </div>
-    )
+// Función para generar un número aleatorio entre min y max
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export default Grafos
+// Generar los nodos con un peso aleatorio
+const nodes = Array.from({ length: 20 }, (_, i) => ({
+    id: storeNames[i],
+    size: getRandomInt(200, 1000), // tamaño aleatorio entre 200 y 1000
+}));
+// Generar los enlaces de manera aleatoria
+const links = [];
+for (let i = 0; i < nodes.length; i++) {
+    const source = nodes[i].id;
+    const target = nodes[getRandomInt(0, nodes.length - 1)].id;
+    links.push({ source, target });
+}
+
+const data = { nodes, links };
+
+const Grafos = () => {
+    const handleClick = (nodeId) => {
+        alert(`Haz clickeado el nodo ${nodeId}`);
+    };
+
+    const config = {
+        nodeHighlightBehavior: true,
+        height: 400,
+        width: 800,
+        staticGraph: false,
+        automaticRearrangeAfterDropNode: true,
+        d3: {
+            gravity: -100, // reducir la fuerza de gravedad
+            linkLength: 50, // reducir la longitud de los enlaces
+            linkStrength: getRandomInt(1,3), // fuerza de los enlaces
+            disableLinkForce: false // habilitar la fuerza de los enlaces
+        },
+        node: {
+
+            size: 120,
+            highlightStrokeColor: 'blue',
+            },
+        };
+    return (
+        <div>
+            <Graph
+                id="graph-id"
+                data={data}
+                config={config}
+                onClickNode={handleClick}
+            />
+        </div>
+    );
+};
+
+export default Grafos;
